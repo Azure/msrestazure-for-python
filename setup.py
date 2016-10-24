282ca624929d6b4a4cc699dcf7c5c4d92b0314e6
Brett Cannon <brett@python.org>
2016-10-24 12:07:18 -0700

    Unpin dependencies in requirements.txt and add keyring

--- setup.py
+++ setup.py
@@ -49,5 +49,6 @@ setup(
         'License :: OSI Approved :: MIT License',
         'Topic :: Software Development'],
     install_requires=[
-        "msrest>=0.4.4"],
+        "msrest>=0.4.4",
+        "keyring>=5.6"],
 )
