e399ec120222a5ffe71fda16ba47665fc54a0630
Laurent Mazuel <laurent.mazuel@gmail.com>
2018-04-18 13:18:37 -0700

    Update msrest dep to 0.4.28

--- setup.py
+++ setup.py
@@ -49,7 +49,7 @@ setup(
         'License :: OSI Approved :: MIT License',
         'Topic :: Software Development'],
     install_requires=[
-        "msrest>=0.4.25,<2.0.0",
+        "msrest>=0.4.28,<2.0.0",
         "keyring>=5.6",
         "adal~=0.5.0"
     ],
